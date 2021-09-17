f5ebe81dc16db8d7cccb76db8da808f6d26b945e
Nooree Choi <nooreechoi916@gmail.com>
2021-09-17 17:44:11 -0400

    Basket Script
    Allows basket to move in accordance to the mouse pointer.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -21,7 +21,7 @@ public class Basket : MonoBehaviour
         mousePos2D.z = -Camera.main.transform.position.z;
 
         //Convert the point from 2D screen space into 3D game world space;
-        Vector3 mousePos3D = Camera.main.ScreentoWorldPoint(mousePos2D);
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
         //Move the x position of this Basket to the x position of the Mouse
         Vector3 pos = this.transform.position;
