8dcf0d3d1814cb408b6756d7c74d91cc2615b755
Nooree Choi <nooreechoi916@gmail.com>
2021-09-17 15:31:15 -0400

    Prefabs and GameObjects Created
    Created all the prefabs and objects for Apple Tree, Apple, Basket, and personalized each GameObject.

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    // Dropping apples every second
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+        // Basic Moment
+        Vector3 pos = transform.position;
+        pos.x += speed* Time.deltaTime;
+        transform.position = pos;
+        // Changing Directions
+   
+    {
+        
+    }
+}
