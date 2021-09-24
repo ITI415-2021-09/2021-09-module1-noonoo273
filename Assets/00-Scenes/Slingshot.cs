a257932e1d32b7d6ec4e6848356e186a5cbc58a1
Nooree Choi <nooreechoi916@gmail.com>
2021-09-24 15:10:33 -0400

    projectile instance appears

--- Assets/00-Scenes/Slingshot.cs
+++ Assets/00-Scenes/Slingshot.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     // Start is called before the first frame update
@@ -25,5 +33,13 @@ public class Slingshot : MonoBehaviour
     {
         print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
